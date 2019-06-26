528a13ce4a65ae584c615f13df09e5a4771245b0
Josh Kelley <joshkel@gmail.com>
2019-06-25 21:35:11 -0400

    Publish

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5a32d26f558e8fc5f113a0465d3989d5.js"
+  "/precache-manifest.4eb394e4aaacf3c781fb23fb2f0083bc.js"
 );
 
 self.addEventListener('message', (event) => {
