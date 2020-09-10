ee0607a38bffc8de4c5c37314d27dc5c44d34ad9
Josh Kelley <joshkel@gmail.com>
2020-09-09 22:06:17 -0400

    Publish

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.1.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.126d27baeb54ec4b914d3d2f9c68a190.js"
+  "/precache-manifest.a9be32a1fbc28040acbc5aa1db2c6a32.js"
 );
 
 self.addEventListener('message', (event) => {
